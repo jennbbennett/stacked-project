bd19ac899468c99af8605e86588e5f4470f32db0
Jennifer Bennett <jennbbennett@hotmail.com>
2015-11-19 11:04:50 -0700

    updated gulpfile to address images and pipe to build properly

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,23 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['html', 'css', 'javascript']);
+gulp.task('default', ['html', 'css', 'javascript', 'images']);
 
 gulp.task('html', function () {
-    return gulp.src('./**/*.html')
+    gulp.src('*.html')
         .pipe(gulp.dest('./build'));
 });
 
 gulp.task('css', function (){
-    return gulp.src('./**/*.css')
+    gulp.src('*.css')
       .pipe(gulp.dest('./build'));
 });
 
 gulp.task('javascript', function(){
-    return gulp.src('./**/*.js')
+    gulp.src('*.js')
       .pipe(gulp.dest('./build'));
+});
+
+gulp.task('images', function() {
+    gulp.src('images/*.jpg')
+    .pipe(gulp.dest('./build/images'));
 });
